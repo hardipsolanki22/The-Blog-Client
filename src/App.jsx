5f276e159cbfcb237980adfea3eea6fc8c82882a
hardipsolanki22 <hardipsolanki2224@gmail.com>
2025-01-25 14:44:05 +0530

    add 3

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,90 @@
+// import React, { useEffect, useState } from 'react';
+// import { Outlet } from 'react-router-dom';
+// import { useQuery } from '@tanstack/react-query';
+// import { ToastContainer } from 'react-toastify';
+// import { SkeletonTheme } from 'react-loading-skeleton';
+// import 'react-loading-skeleton/dist/skeleton.css';
+// import { useDispatch, useSelector } from 'react-redux';
+// import { DNA } from 'react-loader-spinner'
+
+// import Header from './components/Header/Header';
+// import AsideNav from './components/LeftAside/AsideNav'
+// import FooterNav from './components/FooterNav/MobileNavbar';
+// import RightAside from './components/RightAside/RightAside';
+// import getCurrentUser from './components/Api/UserApi/getCurrentUser';
+// import { login, logout } from './featured/authSlice';
+// import { ThemeProvider } from './components/Contexts/theme';
+// import { parseErrorMesaage } from './Helpers/parseErrMsg';
+
+// function App() {
+//   const [themeMode, setThemeMode] = useState(true);
+//   const [isLoading, setIsLoading] = useState(true)
+//   const dispatch = useDispatch();
+//   const authStatus = useSelector(state => state.auth.status)
+
+//   console.log('status: ', authStatus);
+  
+
+//     useEffect(() => {
+//       getCurrentUser()
+//       .then((user) => {
+//         console.log('user: ', JSON.stringify(user));
+        
+//         console.log('ture;');
+//         if (user) {
+//           dispatch(login({userData: user.data}))
+//         } else {
+//           dispatch(logout())
+//         }
+//       })
+//       .finally(() => setIsLoading(false))
+//     }, [])
+
+//     console.log('status: ', authStatus);
+
+
+//   // Toggle dark mode and light mode 
+//   const toggleMode = () => {
+//     setThemeMode(!themeMode);
+//   };
+
+//   useEffect(() => {
+//     document.querySelector('html').classList.remove("light", "dark");
+//     document.querySelector('html').classList.add(themeMode ? "dark" : "light");
+//   }, [themeMode]);
+
+//   return (
+//     <ThemeProvider value={{ themeMode, toggleMode }}>
+//       {!isLoading ? (
+//         <>
+//           <Header />
+//           <main className={`${themeMode ? 'dark' : 'light'} grid sm:grid-cols-12 `}>
+//             <SkeletonTheme
+//               baseColor={themeMode ? '#313131' : '#E0E0E0'}
+//               highlightColor={themeMode ? '#525252' : '#F0F0F0'}
+//             >
+//               <AsideNav />
+//               <Outlet />
+//               <RightAside />
+//               <FooterNav />
+//             </SkeletonTheme>
+//             <ToastContainer />
+//           </main>
+//         </>
+//       ) : (
+//         <div className='h-screen w-full flex flex-col justify-center items-center'>
+//           <DNA />
+//           <p className='text-2xl font-semibold text-white'>The Blog is waiting</p>
+//         </div>
+//       )}
+//     </ThemeProvider>
+//   );
+// }
+
+// export default App;
+
+
+
 import React, { useEffect, useState } from 'react';
 import { Outlet } from 'react-router-dom';
 import { useQuery } from '@tanstack/react-query';
@@ -55,7 +142,6 @@ function App() {
 
   return (
     <ThemeProvider value={{ themeMode, toggleMode }}>
-      {!isLoading ? (
         <>
           <Header />
           <main className={`${themeMode ? 'dark' : 'light'} grid sm:grid-cols-12 `}>
@@ -71,12 +157,7 @@ function App() {
             <ToastContainer />
           </main>
         </>
-      ) : (
-        <div className='h-screen w-full flex flex-col justify-center items-center'>
-          <DNA />
-          <p className='text-2xl font-semibold text-white'>The Blog is waiting</p>
-        </div>
-      )}
+     
     </ThemeProvider>
   );
 }

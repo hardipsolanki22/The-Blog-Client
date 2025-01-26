117fbc89d998c7dbcc69a1120b850bc48a922e56
hardipsolanki22 <hardipsolanki2224@gmail.com>
2025-01-26 19:16:11 +0530

    proxy backend deploy url

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   server: {
     proxy: {
      "/api": {
-        target: "http://localhost:8000",
+        target: "https://the-blog-api.onrender.com",
         changeOrigin: true,
      }
     }

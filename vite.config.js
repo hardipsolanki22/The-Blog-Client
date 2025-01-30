f791ada5627d79660a77df62a63999e782e4bece
hardipsolanki22 <hardipsolanki2224@gmail.com>
2025-01-30 20:08:21 +0530

    remove proxy

--- vite.config.js
+++ vite.config.js
@@ -4,13 +4,11 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   server: {
-    proxy: {
+    // proxy: {
     //  "/api": {
-    //     target: "https://blog-api-d2uc.onrender.com",
-    //     changeOrigin: true,
-    //     secure: true, 
+    //     target: "http://localhost:8000",
     //  }
-    }
+    // }
   },
   plugins: [react()],
 })
